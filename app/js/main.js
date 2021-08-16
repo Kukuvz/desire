15492c996f39b7e4ddc96d0fbe3abdeba741b8a0
Anton Gaivoronskiy <gaivoronskiy.anton.i@gmail.com>
2021-08-16 18:01:09 +0300

    continue adaptation

--- app/js/main.js
+++ app/js/main.js
@@ -64,4 +64,9 @@ $(function () {
         $('.menu').toggleClass('menu--open');
     });
 
+    //media 650px adaptation works-path__items
+    if($(window).width() < 651) {
+        $('.works-path__item--measurements').appendTo('.works-path__items-box');
+    }
+
 })
